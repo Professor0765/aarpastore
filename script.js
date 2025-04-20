21f5e97b6db41dcafdae99ff0532c6208f6548b0
Professor0765 <pawanshrestha2024@gmail.com>
2025-04-20 08:01:20 +0545

    new

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// Script for navigation bar
+$(document).ready(function () {
+    $("a").on("click", function (event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+        $("html, body").animate(
+          {
+            scrollTop: $(hash).offset().top,
+          },
+          800,
+          function () {
+            window.location.hash = hash;
+          }
+        );
+      }
+    });
+  });
+  
+  $(".menu-items a").click(function () {
+    $("#checkbox").prop("checked", false);
+  });
