435b8da3accbdfc2def9556fd658071fcff44a2d
Professor0765 <pawanshrestha2024@gmail.com>
2025-12-26 22:18:52 +0545

    po

--- script.js
+++
@@ -1,9 +0,0 @@
-
-//Menu Button Toggle
-
-const mobile_nav = document.querySelector('.menu-nav');
-const nav_header = document.querySelector('.header')
-const toggleNavbar = () => {
-	nav_header.classList.toggle('active')
-}
-mobile_nav.addEventListener('click' , () => toggleNavbar())
